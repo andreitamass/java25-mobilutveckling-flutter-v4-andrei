fd39692baab801aa1c8ed8d80029759ba58a803b
andreitamas <andrei.tamass83@gmail.com>
2026-09-20 18:24:58 +0200

    added switch widget to change betweeen manga and comic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'details_page.dart';
 
-//StatlessWidget need due to the state of app not needing change
-class HomePage extends StatelessWidget {
+//StatlessWidget need due to the state of app can change
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+  }
+  //Manga i chosen from the beginning
+  class _HomePageState extends State<HomePage> {
+  bool isManga = true;
 
   @override
   Widget build(BuildContext context) {
@@ -14,58 +21,79 @@ class HomePage extends StatelessWidget {
         title: Text('ComicReader'),
       ),
       //Creates card that hold the title and children information and image in the center
-      body: Center(
-        child: Card(
-          child: Column(
+            body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              //Fontsize for text and style
-              Text(
-                'Naruto',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                ),
-              ),
-              //Image from internet
-              Image.network(
-                'https://m.media-amazon.com/images/I/8141AtkCPLL._SL1500_.jpg',
-                width: 200,
-                height: 200,
-              ),
-              //The space between text and card
-              SizedBox(height: 10),
-
-              //Restricting descriptions to a widht of 300
-              SizedBox(
-                width: 300,
-                child: Text(
-                  'Naruto is a famous Japanese manga and anime series created by Masashi Kishimoto. It is an epic adventure about ninjas, friendship, and growing up'
-                ),
-              ),
-
-              //Creates button which takes user to another site where DetailsComic will open
-              //Flutter Navigator, Interactive widget
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailsComic(),
-                    ),
-                  );
-                },
-                child: Text('More Information'),
-              ),
-              //Interactive widget which will add comic to favorites later
-              IconButton(
-                onPressed: () {
-                  print('Comic added to favorites');
-                },
-                icon: Icon(Icons.star),
-              ),
-            ],
+          Text(isManga ? 'Manga' : 'Comics'),
+          //Switch to choose between Manga and Comics
+          //Interactive widget thats swaps from Manga to Comics
+          Switch(
+            value: isManga,
+            onChanged: (bool value) {
+              setState(() {
+                isManga = value;
+              });
+            },
           ),
-        ),
+          //Comic card
+          Expanded(
+            child: Center(
+              child: Card(
+                child: Column(
+                  children: [
+                    // Font size for text and style
+                    Text(
+                      'Naruto',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    //Image from internet
+                    Image.network(
+                      'https://m.media-amazon.com/images/I/8141AtkCPLL._SL1500_.jpg',
+                      width: 200,
+                      height: 200,
+                    ),
+
+                    //Space between text and card
+                    SizedBox(height: 10),
+
+                    //Restricting description to a width of 300
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        'Naruto is a famous Japanese manga and anime series created by Masashi Kishimoto. It is an epic adventure about ninjas, friendship, and growing up',
+                      ),
+                    ),
+
+                    //Button that opens the details page
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsComic(),
+                          ),
+                        );
+                      },
+                      child: Text('More Information'),
+                    ),
+
+                    //Favorite button
+                    IconButton(
+                      onPressed: () {
+                        print('Comic added to favorites');
+                      },
+                      icon: Icon(Icons.star),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),  
     );
   }
